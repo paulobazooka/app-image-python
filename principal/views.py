86626b9c4f4282c75ce41be38f887843b2585304
Paulo Sérgio do Nascimento <pasergio@ic.unicamp.br>
2020-07-13 15:31:45 -0300

    inserção do nome do arquivo no input

--- principal/views.py
+++ principal/views.py
@@ -1,12 +1,4 @@
-
-from django.core.files.base import ContentFile
-from django.shortcuts import render
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from .image_service import *
-from io import BytesIO
 from django.http import HttpResponse
-
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.shortcuts import render
 
 from .image_service import *
@@ -19,13 +11,13 @@ def effect(request):
     if request.method == 'POST' and request.FILES['inputFile'] and request.POST['effect']:
 
         filename = request.FILES['inputFile'].name[:-4]
-
-        print(filename)
         image = pill(request.FILES['inputFile'], request.POST['effect'])
         response = HttpResponse(image, content_type='application/*')
         response['Content-Disposition'] = f'attachment; filename="{filename}.png"'
 
         return response
 
+    else:
+        return render(request, 'index.html')
 
 
